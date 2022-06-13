9e6e1bcd12635d0ed15b3e99689293a0c89cd3af
Emmanuel Uchenna <eunithelp@gmail.com>
2022-06-13 05:58:08 +0100

    Feat: Set up basic CORS in app

--- Flask-CORS/app.py
+++ Flask-CORS/app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-@app.route('/')
-def index():
-		return render_template('index.html')
+def create_app(test_config=None):
+    app = Flask(__name__)
+#   CORS(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-if __name__ == '__main__':
-		app.run(host='127.0.0.1', port=8000, debug=True)
- 
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+
+    @app.route('/messages')
+    @cross_origin()
+    def get_messages():
+        return 'GETTING MESSAGES'
